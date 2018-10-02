7466bf438443302293bbd6a64c73590eec065ce4
Agustí <afebrerp@opentrends.net>
2018-10-02 14:30:33 +0200

    Added Kdoc to ReachAbilityManager

--- presentation/src/main/java/com/example/miquelcastanys/cleanlearning/view/base/ReachAbilityManager.kt
+++ presentation/src/main/java/com/example/miquelcastanys/cleanlearning/view/base/ReachAbilityManager.kt
@@ -18,49 +18,70 @@ import com.example.domain.interactor.CheckInternetConnectionUseCase
 
 object ReachAbilityManager : BroadcastReceiver() {
 
-
+    /**UseCases*/
     private var useCaseBackEnd = CheckInternetConnectionUseCase(ReachAbilityImpl())
     private var useCaseDevices = CheckDevicesReachAbilityUseCase(ReachAbilityDevicesImpl())
 
+    /**BackOffice Address with its listener and its block setter*/
     private var backOfficeAddress = InternetAddress("")
     private lateinit var primaryHostListener: (Boolean) -> Unit
 
+    /**
+     * @param host the url to check or evaluate the back office reach ability
+     * @param block function to set and later on answer the state changes
+     * */
     fun setBackOfficeReachAbleListener(host: String, block: (Boolean) -> Unit) {
         backOfficeAddress = InternetAddress(host)
         primaryHostListener = block
     }
 
-    //Most likely to be a VUE
+    /**First device UDP logic Address with its listener and its block setter
+     * VUE
+     * */
     private const val BROADCAST_PORT_VUE = 49153
     private var firstDevice = InternetAddress("")
     private lateinit var firstDeviceHostListener: (Boolean) -> Unit
+    var checkFirstDeviceEnabled = false //boolean to enable disable checking the host
 
-    var checkFirstDeviceEnabled = false
-
+    /**
+     * @param host the url to check or evaluate the VUE reach ability
+     * @param block function to set and later on answer the state changes
+     * */
     fun setFirstDeviceReachAbleListener(host: String, block: (Boolean) -> Unit) {
         firstDevice = InternetAddress(host, BROADCAST_PORT_VUE)
         firstDeviceHostListener = block
     }
 
-    //Most lilkely to be a FUSE
+    /**Second device UDP logic Address with its listener and its block setter
+     * FUSE
+     * */
     private const val BROADCAST_PORT_FUSE = 8888
     private var secondDevice = InternetAddress("")
     private lateinit var secondDeviceHostListener: (Boolean) -> Unit
+    var checkSecondDeviceEnabled = false//boolean to enable disable checking the host
 
-    var checkSecondDeviceEnabled = false
-
+    /**
+     * @param host the url to check or evaluate the FUSE reach ability
+     * @param block function to set and later on answer the state changes
+     * */
     fun setSecondDeviceReachAbleListener(host: String, block: (Boolean) -> Unit) {
         secondDevice = InternetAddress(host, BROADCAST_PORT_FUSE)
         secondDeviceHostListener = block
     }
 
 
+    /**
+     * @param context general Context to register the receiver
+     * */
     fun registerReceiver(context: Context?) {
         context?.registerReceiver(this, IntentFilter(WifiManager.NETWORK_STATE_CHANGED_ACTION))
     }
 
+    /**Var to evaluate if the network is connected or not*/
     private var isNetworkStateConnected: Boolean = false
 
+
+    /**@Override method to receive connectivity changes from system Broadcast */
     override fun onReceive(context: Context?, intent: Intent?) {
         val extras = intent?.extras
         if (extras != null) {
@@ -73,7 +94,7 @@ object ReachAbilityManager : BroadcastReceiver() {
                         if (!isNetworkStateConnected) {
                             isNetworkStateConnected = true
                             checkHostBackOffice()
-                            //checkDevicesArray()
+                            //checkDevicesArray() //possible call instead of the tick every X seconds.
                         }
                     }
 
@@ -110,6 +131,11 @@ object ReachAbilityManager : BroadcastReceiver() {
         }
     }
 
+    /**
+     * Through the use case checks the reachability of the BO and using the block function calls back to
+     * the the BaseActivity.
+     * Will only inform in changes, not if the same state as previous is received.
+     * */
     private fun checkHostBackOffice() {
 
         useCaseBackEnd.execute(ReachAbilityCallParams(InternetAddress(backOfficeAddress.host)), {
@@ -124,7 +150,11 @@ object ReachAbilityManager : BroadcastReceiver() {
         })
     }
 
-
+    /**
+     * Through the use case checks the reachability of the Devices Array and using the block function calls back to
+     * the the BaseActivity.
+     * Will only inform in changes, not if the same state as previous is received for each devices/host
+     * */
     fun checkDevicesArray() {
 
         useCaseDevices.execute(ReachAbilityDeviceCallParams(buildArrayFromHosts()), { entity ->
@@ -154,6 +184,9 @@ object ReachAbilityManager : BroadcastReceiver() {
         })
     }
 
+    /**
+     * Build an array of host to send to the use case params of the Devices logic.
+     * */
     private fun buildArrayFromHosts(): ArrayList<InternetAddress> {
         val arrayListHost: ArrayList<InternetAddress> = arrayListOf()
         if (!firstDevice.host.isEmpty() && checkFirstDeviceEnabled) {
@@ -165,9 +198,14 @@ object ReachAbilityManager : BroadcastReceiver() {
         return arrayListHost
     }
 
+
+    /**
+     * This logic is pending to be approved and tested.
+     * It checks the devices reach ability every 2 seconds
+     * */
     fun launchVUEListener() {
         val period = Long.MAX_VALUE
-        val countDownInterval = 60000L
+        val countDownInterval = 2000L
 
         object : CountDownTimer(period, countDownInterval) {
             override fun onTick(millisUntilFinished: Long) {
